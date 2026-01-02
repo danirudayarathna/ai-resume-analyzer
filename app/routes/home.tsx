1c5bf8e464d598b7fa819cac95d00575d1a1f852
Daniru Dayarathna <168005576+danirudayarathna@users.noreply.github.com>
2026-01-02 15:50:53 +0530

    home page

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,21 @@
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "CVaid" },
+    { name: "description", content: "Smart way to improve your Resume!" },
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+    return <main className={"bg-[url('/images/bg-main.svg')] bg-cover"}>
+        <section className={"main-section"}>
+            <div className={"page-heading"}>
+                <h1>Track Your Application & Resume Ratings</h1>
+                <h2>Review your submissions and check AI-Powered feedback</h2>
+            </div>
+
+        </section>
+    </main>;
 }
