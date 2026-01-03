f2261c56d3a1e104b573a9778035348fd8016b54
Daniru Dayarathna <168005576+danirudayarathna@users.noreply.github.com>
2026-01-03 20:17:32 +0530

    Navbar second Commit

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,8 +1,16 @@
 import React from "react";
+import {Link} from "react-router";
 
 const Navbar: () => React.JSX.Element = () => {
     return (
-        <div>Navbar</div>
+        <div>
+            <Link to="/">
+                <p className={"text-2xl font-bold text-gradient"}>CVaid</p>
+            </Link>
+            <Link to={"/upload"}>
+                Upload CV
+            </Link>
+        </div>
     )
 }
 export default Navbar;
